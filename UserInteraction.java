55218a325a6a8a5786ada76061553b6795fc18df
prswain <priyadarshini.swain@capgemini.com>
2016-07-14 16:57:43 +0530

    Add files via upload

---
+++ UserInteraction.java
@@ -0,0 +1,31 @@
+package com.flp.ems.view;
+
+import java.util.Scanner;
+
+public class UserInteraction {
+
+
+		static void addEmployee(){
+			Scanner scn=new Scanner(System.in);
+			System.out.println("Enter the name of the employee");
+			String name =scn.next();
+			
+			
+					
+		}
+		static void modifyEmployee(){
+			
+	}
+		static void removeEmployee(){
+		
+		}
+		static void searchEmployee(){
+		
+		}
+		static void getallEmployee(){
+			
+		}
+		
+
+		
+}
